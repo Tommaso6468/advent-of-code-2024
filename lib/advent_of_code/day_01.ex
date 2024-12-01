9bbb7fd396f00d12358cbca9230df547af5d01c2
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2024-12-01 11:22:53 +0100

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Day01 do
   def part1(_args) do
+    {left, right} = parse_input()
+
+    sorted_left = Enum.sort(left)
+    sorted_right = Enum.sort(right)
+
+    Enum.zip(sorted_left, sorted_right)
+    |> Enum.map(fn {l, r} -> abs(l - r) end)
+    |> Enum.sum()
+
   end
 
   def part2(_args) do
+    {left, right} = parse_input()
+
+    frequencies = Enum.frequencies(right)
+
+    Enum.reduce(left, 0, fn num, acc ->
+      count = Map.get(frequencies, num, 0)
+      acc + (num * count)
+    end)
+  end
+
+  defp parse_input() do
+    input = AdventOfCode.Input.get!(1, 2024)
+    |> String.split("\n", trim: true)
+    |> Enum.reduce({[], []}, fn line, {left, right} ->
+      [l, r] = String.split(line) |> Enum.map(&String.to_integer/1)
+      {[l | left], [r | right]}
+    end)
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -5,17 +5,31 @@ defmodule AdventOfCode.Day01Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    """
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
   @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    """
     result = part2(input)
 
-    assert result
+    assert result == 31
   end
 end
